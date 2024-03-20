7f271d069edb62fe0f6839c686954a3cf4ca6129
Sébastien <seb.nicot@gmx.fr>
2024-03-20 08:53:36 +0100

    comparaison

---
+++ compare.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int a = 3;
+    int b = 5;
+    if (a < b)
+    {
+        printf("%d", b);
+    }
+    else
+    {
+        printf("%d", a);
+    }
+    return 0;
+};
